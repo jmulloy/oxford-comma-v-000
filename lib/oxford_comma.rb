28950907d922bd04ec1ee004531e011cabf6142c
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-17 20:36:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(" and ")
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    elements = array.pop
+    new_array = array.join(", " ) + ", and ") + elements
+    new_array
+  end
 end
